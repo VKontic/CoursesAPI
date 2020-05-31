148cd5c814f51c14508293adbd0a19cb5ac353bf
amin-o <aminodzic@protonmail.com>
2020-05-31 22:06:54 +0200

    adding shebangs, all teacher api operations implemented, set userFindAndModify to false to remove deprication warnings.'

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-require('dotenv').config()
+#!/usr/bin/env node
+
 const mongoose = require('mongoose');
 const express = require('express');
 const { json } = require('body-parser');
-const Teacher = require("./controllers/teacherController");
+
+//controllers
+const teacherController = require("./controllers/teacherController");
 const courseController = require("./controllers/courseController");
 
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 
+require('dotenv').config()
+
 const app = express();
 app.use(json());
 
-
+//courses
 app.post('/courses', courseController.create);
 app.delete('/course/:name', courseController.deleteOne);
 app.get('/course/:name', courseController.findByName);
@@ -26,7 +31,19 @@ app.put('/course_id/:id', courseController.updateById);
 app.put('/subs_dec/:id', courseController.decSubs); //decrement course subs by 1 
 app.put('/subs_inc/:id', courseController.incSubs); //decrement course subs by 1
 
-mongoose.connect(process.env.DB_URL)
+//teacher
+app.post('/teacher', teacherController.create);
+app.get('/teacher', teacherController.directSearchByUrl)
+app.delete('/teacher', teacherController.deleteCourse)
+app.put('/teacher/', teacherController.updateCourse);
+app.get('/teachers', teacherController.getAll);
+app.get('/teacher/:username', teacherController.partialSearchByUsername);
+app.delete('/teacher/:username', teacherController.deleteByUsername);
+app.put('/teacher/:username', teacherController.updateUsername);
+
+
+
+mongoose.connect(process.env.DB_URL, { useFindAndModify: false })
     .then(() => {
         app.listen(process.env.PORT, (err) => {
             err ? console.log("Server didnt start", err) : console.log("Server started on port:", process.env.PORT);
--- models/courses.js
+++ models/courses.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const mongoose = require('mongoose');
 
 courseSchema = new mongoose.Schema({
