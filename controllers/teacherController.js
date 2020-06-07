8ee41fbd27530db0536402b1d51f4f843a67803d
amin-o <aminodzic@protonmail.com>
2020-06-07 18:28:59 +0200

    added auth verify on getAll taechers

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -22,26 +22,38 @@ function hashPassword(password) {
 }
 
 exports.create = async (req, res) => {
+
     try {
         //Create a teacher from body
         if (Object.keys(req.query).length === 0) {
+
             if (validateCreateTeacherPayload(req.body)) {
+
                 req.body['password'] = hashPassword(req.body['password']);
                 const result = await Teacher.create(req.body);
                 res.status(200).json(result);
+
             } else {
                 res.status(500).json({ 'error': 'Teacher payload validation failed!' });
             }
+
         } else if (req.query.username !== undefined && req.query.id !== undefined) {
             //add unique course to the teacher
-            const result = await Teacher.findOneAndUpdate({ username: req.query.username }, { $addToSet: { course: req.query.id } });
+            const result = await Teacher.findOneAndUpdate(
+                { username: req.query.username },
+                { $addToSet: { course: req.query.id } }
+            );
+
             res.status(200).json(result);
+
         } else {
             res.status(404).json({ 'error': 'No data found!' });
         }
+
     } catch (err) {
         res.status(500).json({ 'error': 'Unable to create teacher with those parameters.Try different username or email address.' });
     }
+
 }
 
 exports.getAll = async (req, res) => {
@@ -65,24 +77,30 @@ exports.partialSearchByUsername = async (req, res) => {
 //get user who has the username and course id specified in url
 //tweak this to get course data only maybe?
 exports.directSearchByUrl = async (req, res) => {
+
     try {
+
         if (req.query.username !== undefined && req.query.id !== undefined) {
 
             const result = await Teacher.findOne({
                 username: req.query.username,
                 course: req.query.id
             });
+
             res.status(200).json(result);
+
         } else {
             res.status(404).json({ 'error': 'No data found!' });
         }
     } catch (err) {
         res.status(500).json(err);
     }
+
 }
 
 exports.deleteByUsername = async (req, res) => {
     try {
+
         const result = await Teacher.deleteOne({ username: req.params.username });
 
         if (result.deletedCount != 0) {
@@ -90,17 +108,26 @@ exports.deleteByUsername = async (req, res) => {
         } else {
             res.status(404).json({ "error": "Teacher not found!" });
         }
+
     } catch (err) {
         res.status(500).json(err);
     }
+
 }
 
 exports.deleteCourse = async (req, res) => {
     try {
 
         if (req.query.username !== undefined && req.query.id !== undefined) {
-            const result = await Teacher.findOneAndUpdate({ username: req.query.username }, { $pull: { course: req.query.id } }, { new: true });
+
+            const result = await Teacher.findOneAndUpdate(
+
+                { username: req.query.username },
+                { $pull: { course: req.query.id } },
+                { new: true });
+
             res.status(200).json(result);
+
         } else {
             res.status(404).json({ 'error': 'No data found!' });
         }
@@ -109,17 +136,61 @@ exports.deleteCourse = async (req, res) => {
     }
 }
 
+exports.deleteCourseFromTeacher = async (req, res) => {
+    try {
+        
+        if (req.query.username !== undefined && req.query.id !== undefined) {
+
+            const result = await Teacher.findOneAndUpdate(
+
+                { username: req.query.username },
+                { $pull: { course: req.query.id } },
+                { new: true });
+
+            res.status(200).json(result);
+
+        } else {
+            res.status(404).json({ 'error': 'No data found!' });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+
 //get user with requested course and update the course with body payload
 exports.updateCourse = async (req, res) => {
+
     if (req.query.username !== undefined && req.query.id !== undefined) {
-        Promise.all([Teacher.findOneAndUpdate({ username: req.query.username }, { $set: { course: req.body.id } }, { new: true }), Teacher.findOneAndUpdate({ username: { $ne: req.query.username }, course: req.body.id }, { $set: { course: req.query.id } }, { new: true })]).then(results => {
+
+        Promise.all([Teacher.findOneAndUpdate(
+            
+            { username: req.query.username },
+            { $set: { course: req.body.id } },
+            { new: true })
+            
+            ,Teacher.findOneAndUpdate(
+
+                { username: { $ne: req.query.username }, course: req.body.id },
+                { $set: { course: req.query.id } },
+                { new: true }
+                
+            )]).then(results => {
+
             res.status(200).json({ "success": "Successfully updated teacher's course!" });
+
         }).catch(err => {
+
             res.status(500).json({ "error": "Bad parameters!" });
+
         })
+
     } else {
+
         res.status(404).json({ 'error': 'No data found!' });
+
     }
+
 }
 
 exports.updateByUsername = async (req, res) => {
